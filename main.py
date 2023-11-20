cebee1312423cdb9f6463ca75bcdc4d2185521e2
Aleksey D <asdzog@mail.ru>
2023-11-20 07:57:21 +0400

    edit web_app

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
-from page_content import html_content
 
 
 hostname = 'localhost'
 serverPort = 8080
 
-
 class MyServer(BaseHTTPRequestHandler):
-
-    @staticmethod
-    def __get_html_content():
-        return html_content
 
     def do_GET(self):
         query_components = parse_qs(urlparse(self.path).query)
-        with open("index.html", "r", encoding='utf-8') as f:
-            page_content = f.read()
+        with open("index.html", "r", encoding='utf-8') as file:
+            page_content = file.read()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
